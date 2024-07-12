a2f2e3f6e1ea6cb4085f5a54dded5f230f694433
Tim Howard <tdhoward@users.noreply.github.com>
2024-07-11 22:54:38 -0700

    Minor change to help files

--- dist/files/help/test.py
+++
@@ -1,6 +0,0 @@
-# Blink an LED
-import machine
-
-while True:
-    # toggle pin
-    pass
---
+++ dist/files/user/hello.py
@@ -0,0 +1,7 @@
+# This is an example file.
+# Feel free to modify or delete it!
+
+print('Hello, world!')
+
+# Yeah, that's it.  Not very exciting, is it?
+# Check out the help section for more interesting examples!
